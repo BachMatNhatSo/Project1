f91cebf2c4a9b5abfb2c100c0282874e171e591b
DuyNhan <pnduynhan@gmail.com>
2023-09-02 12:28:16 +0700

    xay dung ra mvc

--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,17 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('hello world')
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+
+    router.get('/map1', (req, res) => {
+        return res.send('hello world map1')
     });
+
+
+
     return app.use("/", router);
 }
 module.exports = initWebRoutes;
