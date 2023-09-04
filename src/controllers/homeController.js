b231f01c4b03658ce10519cc0f77c156fc743165
DuyNhan <pnduynhan@gmail.com>
2023-09-04 13:37:52 +0700

    crud c user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,10 @@
 
 import db from '../models/index';
-
+import CRUDServices from '../services/CRUDServices';
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
         return res.render('homepage.ejs', { data: JSON.stringify(data) });
-
     } catch (e) {
         console.log(e);
     }
@@ -13,8 +12,18 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
+let getCRUDPage = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req, res) => {
+    let message = await CRUDServices.createNewUser(req.body);
+    console.log(message);
+    return res.send('this is crud page');
+}
 
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUDPage: getCRUDPage,
+    postCRUD: postCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,10 +5,8 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
-
-    router.get('/map1', (req, res) => {
-        return res.send('hello world map1')
-    });
+    router.get('/crud', homeController.getCRUDPage);
+    router.post('/post-crud', homeController.postCRUD);
 
 
 
