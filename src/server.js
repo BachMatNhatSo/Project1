8e53bc6fd957ae521dc3b49a36c76e066d33c277
DuyNhan <pnduynhan@gmail.com>
2023-09-07 15:03:45 +0700

    check login be

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-import connectDB from "./config/connectDB"
+import connectDB from "./config/connectDB";
+import cors from 'cors';
 require('dotenv').config();
 
 let app = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
